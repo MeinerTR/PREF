8e6ad5fbf883b18e326fc38a65bb61fd87087c04
MeinerTR <87811811+MeinerTR@users.noreply.github.com>
2022-05-12 15:07:23 +0300

    [FIXED]
    Fixed only by changing the "malloc" to "calloc"...

--- pref.c
+++ pref.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define pUI unsigned int
 #define pCUI const unsigned int
 #define pUC unsigned char
 #define pCUC const unsigned char
 
-typedef enum {True, False} Bool;
-#define _ERROR(MSG) {perror(#MSG); exit(42);}
-
-static pCUI pLength(pCUC *_STR7) {
-    pUI _Output7 = 0;
-    while (1) {
-        if (_STR7[_Output7] == '\0') {
-        break; } else { _Output7++; }
-    } return _Output7;
-}
-
 pUC *prefGet(pCUC *_Path7, pCUC *_Key7) {
     FILE *_File7 = fopen(_Path7, "r");
     if (!_File7) { return NULL; }
-    Bool _ADD7 = False, _Hope7 = True;
+    pUI _ADD7 = 0; bool _Hope7 = true;
     pUI _CURR7 = 0, _CURR5 = 0;
-    pUC *_Output7 = (pUC *) malloc (
-        sizeof(pUC) ); char _CHR7;
-    // pCUI _LEN7 = pLength(_Key7);
+    pUC *_Output7 = (pUC *) calloc (
+        1, sizeof(pUC) ); char _CHR7;
     while (1) { _CHR7 = fgetc(_File7);
         if (_CHR7 == '\n') {
-            if (_ADD7 == True) {
+            if (_ADD7 == true) {
                 return _Output7;
-            } _Hope7 = True; _CURR7 = 0;
+            } _Hope7 = true; _CURR7 = 0;
         } else if (_CHR7 == EOF) {
-            if (_ADD7 == True) { return _Output7; } break; 
-            } else {
-                if (_ADD7 == True) {
+            if (_ADD7 == true) { return _Output7; } break; 
+        } else {
+                if (_ADD7 == true) {
                     _Output7 = realloc (
                     _Output7, sizeof(pUC) * _CURR5 + 1
                     ); _Output7[_CURR5] = _CHR7; _CURR5++;
-                } else if (_Hope7 == True) {
+                } else if (_Hope7 == true) {
                     if (_CHR7 != _Key7[_CURR7]) {
-                        _Hope7 = False;         }
-                    if (_CHR7 == ' ') { //  & _CURR7 == _LEN7
-                        _ADD7 = True; } else {_CURR7++; }
+                        _Hope7 = false;         }
+                    if (_CHR7 == ' ') {
+                        _ADD7 = true; } else {_CURR7++; }
                 }
         }
     } fclose(_File7);
@@ -51,32 +40,33 @@ pUC *prefGet(pCUC *_Path7, pCUC *_Key7) {
 void prefChange(pCUC *_Path7, pCUC *_Key7, pCUC *_NewVar7, pCUC *_SaveAS7) {
     FILE *_File7 = fopen(_Path7, "r"), *_File5 = fopen("OOPSIE.VIRUS", "w");
     fclose(_File5); _File5 = fopen("OOPSIE.VIRUS", "a");
-    pUI _Length7 = 0, _LEN7 = pLength(_Key7); char _CHR7 = 0; Bool _Search7 = True;
-    pUC *_Line7 = (pUC *) malloc ( sizeof(pUC) * 5 ); Bool _Write7 = True;
+    pUI _Length7 = 0; char _CHR7 = 0; bool _Search7 = true;
+    pUC *_Line7 = (pUC *) malloc ( sizeof(pUC) * 5 ); bool _Write7 = true;
     while (_CHR7 != -2) { _CHR7 = fgetc(_File7);
-        if (_CHR7 == '\n' | _CHR7 == EOF) { // :( --- ); BUGGY ;( --- ):
+        if (_CHR7 == '\n' | _CHR7 == EOF) {
             if (_Line7[4] != 5) {
-                if (_Write7 == True) { 
+                if (_Write7 == 1) { 
                     fputs(_Line7, _File5); fputc('\n', _File5);
                 } else {
-                    _Write7 = True;
+                    _Write7 = 1;
                     fprintf(_File5, "%s %s\n", _Key7, _NewVar7);
-                }  } free(_Line7); 
-            _Line7 = (pUC *) malloc (
-                    sizeof(pUC) * 5);
-            _Length7 = 0; _Search7 = True;
-            if (_CHR7 == EOF) {_CHR7 = -2;}
+                }  }
+            if (_CHR7 == EOF) {_CHR7 = -2;} else {
+                free(_Line7);  _Line7 = (pUC *) malloc (
+                                        sizeof(pUC) * 5);
+                _Length7 = 0; _Search7 = 1;
+            }
         } else {
             if (_Length7 > 4) {
                 _Line7 = realloc(
                 _Line7, _Length7 + 1);
             } _Line7[_Length7] = _CHR7;
-            if (_Search7 == True) {
+            if (_Search7 == true) {
                 if (_CHR7 != _Key7[_Length7]) {
-                    _Search7 = False;
-                } if (_Length7 == _LEN7) {
-                if (_CHR7 == ' ') { _Write7 = False;
-                } } } _Length7++; }
+                    _Search7 = false;
+                }
+                if (_CHR7 == ' ') { _Write7 = false;
+                } } _Length7++; }
     } fclose(_File7); fclose(_File5); free(_Line7);
     remove(_Path7); rename("OOPSIE.VIRUS", _SaveAS7);
 }
@@ -84,14 +74,14 @@ void prefChange(pCUC *_Path7, pCUC *_Key7, pCUC *_NewVar7, pCUC *_SaveAS7) {
 int main(void) {
     pUC *PreStar = prefGet("./General.pref", "STAR"),
         *PrePlanet = prefGet("./General.pref", "PLANET");
-    printf("Before changed!\n");
+    printf("\nBefore changed!\n");
     printf("STAR: %s\nPLANET: %s\n", PreStar, PrePlanet);
-    prefChange("./General.pref", "STAR", "Sun", "General.pref");
+    prefChange("./General.pref", "STAR", "Son", "General.pref");
     prefChange("./General.pref", "PLANET", "Mars", "General.pref");
     free(PreStar); free(PrePlanet);
     pUC *NewStar = prefGet("./General.pref", "STAR"),
         *NewPlanet = prefGet("./General.pref", "PLANET");
-    printf("After changed!\n");
-    printf("STAR: %s\nPLANET: %s\n", NewStar, NewPlanet);
+    printf("\nAfter changed!\n"); // DUDE WTH IS THAT BUG?
+    printf("STAR: %s\nPLANET: %s\n\n", NewStar, NewPlanet);
     free(NewStar); free(NewPlanet);
 }
